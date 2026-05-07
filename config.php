1649185821068c27981d87ec424c1f2c2e91297c
MTNT <mateusznitka01@gmail.com>
2026-05-07 17:26:43 +0200

    Dockerfiles added

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 session_start();
 
 // Database path
-define('DB_PATH', __DIR__ . '/shares.db');
+define('DB_PATH', '/var/lib/bakdrop/shares.db');
 
 // System paths - EDIT IT
 // confirm that www-data user has r/w privileges to your root directory
 // base URL is used in link shares
 define('FILES_PATH', '/fsr');                    // Root directory for all files
-define('BASE_URL', 'http://10.10.99.251');       // Base URL for share links
+define('BASE_URL', 'http://10.10.50.253');       // Base URL for share links
 
 // You can leave it or change to your timezone
 date_default_timezone_set('Europe/Warsaw');
