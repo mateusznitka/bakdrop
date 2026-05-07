4de0a4c8292449b9e65e9ffcbc2c2a0840949b02
MTNT <mateusznitka01@gmail.com>
2026-05-07 20:03:46 +0200

    Changed details in config.php

--- config.php
+++ config.php
@@ -1,15 +1,11 @@
 <?php
-// Database path
-define('DB_PATH', '/var/lib/bakdrop/shares.db');
 
 // System paths - EDIT IT
-// confirm that www-data user has r/w privileges to your root directory
-// base URL is used in share links
+// confirm that www-data user has r/w privileges to your root and DB directory
+define('DB_PATH', '/var/lib/bakdrop/shares.db');    // Database path, it will be created there
 define('FILES_PATH', '/fsr');                    // Root directory for all files
 define('BASE_URL', 'http://10.10.50.253:8080');       // Base URL for share links
 define('DEFAULT_LANG', 'pl');                        // Default language for public pages (en, pl)
-
-// Timezone is used for showing expiration time in share links
-date_default_timezone_set('Europe/Warsaw');
+date_default_timezone_set('Europe/Warsaw');     // Timezone is used for showing expiration time in share links
 
 ?>
