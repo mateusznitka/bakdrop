36593ed955332fc2fa2a28baba9b0453adeff658
MTNT <mateusznitka01@gmail.com>
2026-05-12 00:11:21 +0200

    fix in config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // System paths - EDIT IT
 // confirm that www-data user has r/w privileges to your root and DB directory
 define('DB_PATH',      getenv('DB_PATH')      ?: '/var/lib/bakdrop/shares.db');
-define('FILES_PATH',   getenv('FILES_PATH')   ?: '/path-to-your-data-dir');
+define('FILES_PATH',   getenv('FILES_PATH')   ?: '/fsr');
 define('BASE_URL',     getenv('BASE_URL')     ?: 'https://your-domain-or-ip');
 define('DEFAULT_LANG', getenv('DEFAULT_LANG') ?: 'en');
 date_default_timezone_set(getenv('TZ') ?: 'Europe/Warsaw');
